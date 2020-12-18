2d2c142a2135340a5468ce79a0569564d4f436f8
siddharth sharma <sharma.siddharth1610@gmail.com>
2020-12-18 12:58:02 +0530

    Block2|Accesing and assigning

--- block-BJaaca/code/index.js
+++ block-BJaaca/code/index.js
@@ -1,22 +1,38 @@
 //  Follow instructions below and write code accordingly.
 
 //  - Create a variable named `character` using `let` assign a value of empty object.
+let character = {
+
+}
 
 //  - Go to this [Arya Stark](https://awoiaf.westeros.org/index.php/Arya_Stark)
 
 //  Re-assign the value of `character` variable to and object with the key `characterName` and value of "Arya"
+let character = {
+    characterName:"Arya"
+}
 
 // - A variable named `age` is predefined with value 20. Add a new key named `character-age` with the value of `age` variable i.e 20 (don't use the value 20)
 
-let age = 20;
+
+let character = {
+    characterName:"Arya",
+    characterage:21
+}
 
 // - Add a new key `sur name` with the value of the surname of the character i.e "Stark". (Don't remove the space between `sur` and `name`)
+character.
 
 // - Add a new key `title` with the value title of the character i.e "Lady of Winterfell"
+character.title = "Lady of Winterfell";
 
 // - Add another property named `greet` and value should be a function when called should alert `I am [NAME HERE] and my title is [TITLE HERE]`.
+character.greet = function(){
+    alert(`I am `)
+}
 
 // - Add a new property to the character object named `isFemale` and value will be `true`
+character.isFemale = true;
 
 // - Execute the function stored in the greet key of the character object
 
@@ -25,6 +41,11 @@ let age = 20;
 // - Check by calling `greet` and see if the message changed or not.
 
 //  - Add a new property in the character object `isAdult`. The value will be true if age is greater than 18 or it will be false.
+if(age>18){
+    character.isAdult = true;
+} else{
+    character.isAdult = false;
+}
 
 // - Add a new method (function inside objects are called methods) named `changeIsAdult`. When called should be able to flip the value of `isAdult` from true to false and false to true.
 
