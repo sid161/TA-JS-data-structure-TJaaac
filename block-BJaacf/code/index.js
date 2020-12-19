c86c755182925977ec8ae3c638a5d3ca0eeb8536
siddharth sharma <sharma.siddharth1610@gmail.com>
2020-12-19 23:30:55 +0530

    Block3|Arrays

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,14 +1,28 @@
 // 1. Create an array named numbers and store 5 number values in it
+let numbers = [1,2,3,4,5];
 
 // 2. Calculate the sum of array items and print it to the console using console.log()
+console.log(
+    numbers.reduce((a, b) => a + b, 0)
+  )
 
 // 3. Calculate the average of array items and print it to the console using console.log()
+const sum = times.reduce((a, b) => a + b, 0);
+const avg = (sum / times.length) || 0;
+
+console.log(`The sum is: ${sum}. The average is: ${avg}.`);
 
 // 4. Find the highest number in the array and print it to the console using console.log()
 
 // 5. Find the lowest number in the array and print it to the console using console.log()
 
 // 6. Find the even numbers in the array and print them to the console using console.log()
+let even = [];
+for(let number of numbers){
+    if(number % 2 == 0){
+        console.log(`${number}`);
+    }
+}
 
 // 7. Find the odd numbers in the array and print them to the console using console.log()
 
