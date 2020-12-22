b5cb1def6566bc34cdbcea7dfe5b8e62700513a1
siddharth sharma <sharma.siddharth1610@gmail.com>
2020-12-22 13:03:43 +0530

    assignment2|Block3

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,6 +3,7 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
+let collection = sumArray(numbers, userIds) 
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
 
